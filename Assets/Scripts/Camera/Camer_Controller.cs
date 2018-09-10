775ac10864d5793ca4028bfdd75f382f41205e98
unknown <deathbug67@hotmail.com>
2018-09-11 01:53:25 +0300

    f*cked up grids more, added camera controller

---
+++ Assets/Scripts/Camera/Camer_Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camer_Controller : MonoBehaviour {
+    public GameObject followTarget;
+    private Vector3 targetPos;
+    public float movespeed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, movespeed * Time.deltaTime);
+
+		
+	}
+}
