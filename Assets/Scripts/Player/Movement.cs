57428841c2b80914e8dd3d7d951fefc5089d1a05
unknown <deathbug67@hotmail.com>
2018-09-10 22:49:12 +0300

    added movement script

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
+    public float movementSpeed;
+
+
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,9 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        float moveHorizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float moveVertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        transform.Translate(new Vector3(moveHorizontal, moveVertical));
+
+    }
 }
