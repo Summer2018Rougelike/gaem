e1dcdc49875298cf9693c6a2c7effb3cfc50dc51
unknown <deathbug67@hotmail.com>
2018-09-11 00:28:44 +0300

    Animated_Player_Beta

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -1,23 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Movement : MonoBehaviour {
-
-    public float movementSpeed;
-
-
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float moveHorizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
-        float moveVertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-        transform.Translate(new Vector3(moveHorizontal, moveVertical));
-
-    }
-}
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine;  public class Movement : MonoBehaviour {      public float movementSpeed;     private Animator anim;    	// Use this for initialization 	void Start () {         anim = GetComponent<Animator>(); 		 	} 	 	// Update is called once per frame 	void Update () {         float moveHorizontal = Input.GetAxisRaw("Horizontal") * movementSpeed * Time.deltaTime;         float moveVertical = Input.GetAxisRaw("Vertical") * movementSpeed * Time.deltaTime;         transform.Translate(new Vector3(moveHorizontal, moveVertical));         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));      } } 
