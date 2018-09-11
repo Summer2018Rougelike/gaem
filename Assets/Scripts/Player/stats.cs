88fe4b73799b2871c6f2b196a6602966bc7bc175
unknown <deathbug67@hotmail.com>
2018-09-11 20:13:46 +0300

    added stats

---
+++ Assets/Scripts/Player/stats.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public int str;
+ // public int per;
+    public int end;
+ // public int chr;
+ // public int intl;
+    public int agi;
+    public int lck;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
