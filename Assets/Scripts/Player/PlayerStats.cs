797946047ce19c2431cc83d78f305748ef8e3619
unknown <deathbug67@hotmail.com>
2018-09-11 21:11:57 +0300

    combat first steps

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -13,11 +13,21 @@ public class PlayerStats : MonoBehaviour {
     public int lck;
     public int xp;
     public int lvl;
+    public int dmg;
+    public int hp;
+    public int arm;
+    public int atk;
+    public int crt;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+
+        dmg = str;
+        hp = end*3;
+        atk = agi;
+        crt = lck;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
