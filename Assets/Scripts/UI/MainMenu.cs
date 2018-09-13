80a5c8290c8c2a9231238ae91c1307f7ff098d8d
unknown <deathbug67@hotmail.com>
2018-09-13 03:08:40 +0300

    added main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Quit()
+    {
+        Debug.Log("Shut Down");
+        Application.Quit();
+    }
+    public void Startgame()
+    {
+        Debug.Log("Game Started");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+}
